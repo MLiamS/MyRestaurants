01a31a20e6cba049c62b2a9760c2ea6fa338be5a
liam <liamstabeno@gmail.com>
2017-05-31 10:35:18 -0700

    update image quality, add phone, website, location functionality

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -25,11 +25,12 @@ public class Restaurant {
     public Restaurant(String name, String phone, String website,
                       double rating, String imageUrl, ArrayList<String> address,
                       double latitude, double longitude, ArrayList<String> categories) {
+
+        this.mImageUrl = getLargeImageUrl(imageUrl);
         this.mName = name;
         this.mPhone = phone;
         this.mWebsite = website;
         this.mRating = rating;
-        this.mImageUrl = imageUrl;
         this.mAddress = address;
         this.mLatitude = latitude;
         this.mLongitude = longitude;
@@ -70,5 +71,10 @@ public class Restaurant {
 
     public ArrayList<String> getCategories() {
         return mCategories;
+    }
+
+    public String getLargeImageUrl(String imageUrl) {
+        String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 6).concat("o.jpg");
+        return largeImageUrl;
     }
 }
